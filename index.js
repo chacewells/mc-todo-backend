cbc8f4740f41a477c7953b1760ea7abdeb179e5b
chace <chacewells@gmail.com>
2023-07-11 16:48:16 -0700

    Add cors module to handle cross-origin requests; some tweaks to clean up the interaction

--- index.js
+++ index.js
@@ -1,44 +1,46 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 const todoListRepo = require("./repository/todo-list");
 
 
-app.get("/", function(req,res){
+app.get("/", function (req, res) {
     res.send("Hello World");
     res.end();
 });
 
 
-app.get("/todo", function(req,res){
+app.get("/todo", function (req, res) {
     res.json(todoListRepo.getAll());
-    
 });
 
 
-app.post("/todo", function(req,res){
+app.post("/todo", function (req, res) {
     //req.body = {"description": "walked dog"};
     const description = req.body.description;
-    todoListRepo.add(description);
-    res.send(201).end();
+    console.log("got description", {description});
+    const newTodoItem = todoListRepo.add(description);
+    res.json(newTodoItem).location(`http://localhost:3000/todo/${newTodoItem.id}`).send(201).end();
 });
 
-app.get("/todo/:id", function(req,res){
+app.get("/todo/:id", function (req, res) {
     const todoList = todoListRepo.getAll();
     const todoItem = todoList.find(item => item.id == req.params.id);
     res.json(todoItem);
 })
 
-app.put("/todo/:id", function(req,res){
-   const id = req.params.id;
-   console.log("id =" + id);
-   const todoItemsUpdates = req.body;
-   todoListRepo.updateId(id,todoItemsUpdates);
+app.put("/todo/:id", function (req, res) {
+    const id = req.params.id;
+    console.log("id =" + id);
+    const todoItemsUpdates = req.body;
+    todoListRepo.updateId(id, todoItemsUpdates);
     res.sendStatus(200).end()
 })
 
 
-app.listen("3000", function(req,res){
+app.listen("3000", function (req, res) {
     console.log("Server running on port 3000!");
 });
--- repository/todo-list.js
+++ repository/todo-list.js
@@ -1,32 +1,33 @@
 const TodoItem = require("../model/TodoItem");
 
-const todoList = [];
 let nextId = 1;
+const todoList = [];
 
 
-function add(description){
-    const todoItem = new TodoItem(nextId,description,false);
+function add(description) {
+    const todoItem = new TodoItem(nextId, description, false);
     todoList.push(todoItem);
 
     nextId++;
+    return todoItem;
 }
 
 
-function updateId(id,todoItemsUpdates){
-    console.log("typeOf id = " + typeof(id))
-    console.log("type of todoList[0] = "+ typeof(todoList[0].id));
+function updateId(id, todoItemsUpdates) {
+    console.log("typeOf id = " + typeof (id))
+    console.log("type of todoList[0] = " + typeof (todoList[0].id));
     const todoItem = todoList.find(item => item.id == id);
-    if(todoItemsUpdates.description){
+    if (todoItemsUpdates.description) {
         todoItem.description = todoItemsUpdates.description;
     }
-    if(todoItemsUpdates.done != null){
+    if (todoItemsUpdates.done != null) {
         todoItem.done = todoItemsUpdates.done;
     }
     console.log(todoItem);
 }
 
 
-function getAll(){
+function getAll() {
     return todoList;
 }
 
